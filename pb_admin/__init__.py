ad214f2c0f73db74ef9a0acdb701271defdf7138
vcslav-v <donn.nus@gmail.com>
2023-10-31 18:06:26 +0100

    fix

--- pb_admin/__init__.py
+++ pb_admin/__init__.py
@@ -1,7 +1,8 @@
 """Main module pb_admin project."""
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 __author__ = 'Vaclav_V'
 __all__ = ['session']
 
 from pb_admin.main import PbSession as session
+from pb_admin.tags import Tags
