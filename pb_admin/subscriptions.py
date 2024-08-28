28715b068b509ab815d630e1dff400a1f2e86329
vcslav-v <donn.nus@gmail.com>
2024-08-28 13:13:35 +0200

    fix

--- pb_admin/subscriptions.py
+++ pb_admin/subscriptions.py
@@ -28,21 +28,23 @@ class Subscriptions():
                         values['user_id'] = cell['belongsToId']
                     else:
                         values[cell['attribute']] = cell['value']
-
-                subscriptions.append(
-                    schemas.Subscription(
-                        ident=values.get('id'),
-                        subscription_id=values.get('subscription_id'),
-                        status=values.get('status'),
-                        period=values.get('period'),
-                        billing_plan=values.get('billingPlan'),
-                        resubscribe=values.get('resubscribe'),
-                        user_id=values.get('user_id'),
-                        start_date=datetime.fromisoformat(values.get('start_date')) if values.get('start_date') else None,
-                        end_date=datetime.fromisoformat(values.get('end_date')) if values.get('end_date') else None,
-                        updated_at=datetime.fromisoformat(values.get('updated_at')) if values.get('updated_at') else None,
+                try:
+                    subscriptions.append(
+                        schemas.Subscription(
+                            ident=values.get('id'),
+                            subscription_id=values.get('subscription_id'),
+                            status=values.get('status'),
+                            period=values.get('period'),
+                            billing_plan=values.get('billingPlan'),
+                            resubscribe=values.get('resubscribe'),
+                            user_id=values.get('user_id'),
+                            start_date=datetime.fromisoformat(values.get('start_date')) if values.get('start_date') else None,
+                            end_date=datetime.fromisoformat(values.get('end_date')) if values.get('end_date') else None,
+                            updated_at=datetime.fromisoformat(values.get('updated_at')) if values.get('updated_at') else None,
+                        )
                     )
-                )
+                except Exception as e:
+                    pass
             if raw_page.get('next_page_url'):
                 parsed_url = urlparse(raw_page.get('next_page_url'))
                 params.update(parse_qs(parsed_url.query))
