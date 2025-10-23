e3ba028944ac2cd7e14f04cee40ddaf2ad184d97
vcslav-v <donn.nus@gmail.com>
2025-10-23 10:33:42 +0200

    upd

--- pb_admin/user_groups.py
+++ pb_admin/user_groups.py
@@ -135,7 +135,6 @@ class UserGroups():
                 except Exception as e:
                     text = await resp.text()
                     print(f'Error attaching user {user_id} to group {user_group_ident}: {e}\nResponse text: {text}')
-                    raise e
             await sleep(0.1)  # To avoid overwhelming the server
         
         return True
